3c7353da42cb6cafbb000cf3b67dfe2881b736ca
Juan Abregu <juan.abregu@hitss.com>
2026-06-21 21:47:38 -0500

    Implementar mÃetodo Modificar en clase Libro

--- src/com/biblioteca/Libro.java
+++ src/com/biblioteca/Libro.java
@@ -14,40 +14,25 @@ public class Libro {
         this.disponible = true;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public String getTitulo() { return titulo; }
+    public String getAutor() { return autor; }
+    public boolean isDisponible() { return disponible; }
 
-    public String getTitulo() {
-        return titulo;
-    }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setAutor(String autor) { this.autor = autor; }
+    public void setDisponible(boolean disponible) { this.disponible = disponible; }
 
-    public String getAutor() {
-        return autor;
-    }
-
-    public boolean isDisponible() {
-        return disponible;
-    }
-
-    public void setTitulo(String titulo) {
+    // Métodos del UML
+    public void modificar(String titulo, String autor) {
         this.titulo = titulo;
-    }
-
-    public void setAutor(String autor) {
         this.autor = autor;
     }
 
-    public void setDisponible(boolean disponible) {
-        this.disponible = disponible;
-    }
-
     public void mostrarInfo() {
-        System.out.println(
-                "ID: " + id +
-                        " | Título: " + titulo +
-                        " | Autor: " + autor +
-                        " | Disponible: " + disponible
-        );
+        System.out.println("ID: " + id +
+                " | Título: " + titulo +
+                " | Autor: " + autor +
+                " | Disponible: " + disponible);
     }
 }
